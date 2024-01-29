d505e8ffdf5ceee34ac2e9b70316982b3607df45
constdeveloper24 <constdeveloper24@gmail.com>
2024-01-29 19:11:25 +0900

    feat: add services section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import About from "@/components/About";
 import Hero from "@/components/Hero";
 import MobileNav from "@/components/MobileNav";
 import Nav from "@/components/Nav";
+import Services from "@/components/Services";
 import React, { useState } from "react";
 
 const HomePage = () => {
@@ -20,6 +21,8 @@ const HomePage = () => {
         <div className="relative z-[30]">
           {/* About */}
           <About />
+          {/* Services */}
+          <Services />
         </div>
       </div>
     </div>
